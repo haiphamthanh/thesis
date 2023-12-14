c883107229525eef2f0132ba87aa9a6fafa3ad4e
HaiPham <pthai076@icloud.com>
2023-12-14 22:04:38 +0700

    refactor: v2 alpha

--- src/experiments/optimization.ts
+++ src/experiments/optimization.ts
@@ -19,56 +19,40 @@ export class Optimization {
   public onlineMCKP() {
     // TODO: Confirm U K w v C P
     const U = [];
-    const K: { w: number }[] = [];
+    const K: { w: number }[][] = [];
     const w: number[][] = [];
     const v: number[][] = [];
     let C = 1000;
 
     // Definations
     const P: PStruct[] = []; // 𝑃 ← ∅
-    const userNumber = U.length; // |𝑈|
+    const _U_ = U.length; // |𝑈|
     const picked_d: { d: number; i: number }[] = [];
 
     // Process
-    for (let i = 0; i < userNumber; i++) {
+    for (let i = 0; i < _U_; i++) {
       // TODO
       // 𝐷𝑖 ← dominant items of 𝐾𝑖 sorted by increasing weight
       const Di: number[] = [];
 
       for (let d = 0; d < Di.length; d++) {
         // Compute incremental values and weights (𝑣𝑖𝑑,𝑤𝑖𝑑 )
-        let v_id = v[i][d];
-        let w_id = w[i][d];
-
-        if (d === 0) {
-          v_id = v[i][0];
-          w_id = w[i][0];
-        } else {
-          v_id = v[i][d] - v[i][d - 1];
-          w_id = w[i][d] - w[i][d - 1];
-        }
+        const incremental = this.computeIncremental(v, w, i, d);
+        let v_id = incremental.v_id;
+        let w_id = incremental.w_id;
 
         // Compute efficiency angle 𝜃𝑖𝑑 :
-        let θ_id = -1;
+        const θ_id = this.computeEfficiencyAngle(v_id, w_id);
 
-        if (v_id === 0 && w_id === 0) {
-          θ_id = (3 * Math.PI) / 2;
-        } else if (v_id < 0 && w_id <= 0) {
-          θ_id = 2 * Math.PI + Math.atan2(v_id, w_id);
-        } else {
-          θ_id = Math.atan2(v_id, w_id);
-        }
-
+        // 𝑃 ← 𝑃 ∪ (𝜃𝑖𝑑,𝑤𝑖𝑑 )
         P.push({ θ: θ_id, w: w_id, i, d });
       }
 
       // Get updated efficiency threshold 𝜃∗:
-      // 𝜃∗ ← A𝑙𝑔𝑜𝑟𝑖𝑡ℎ𝑚1 (𝑃,𝐶,𝑖, |𝑈 |)
-      const a_ = this.algorithrm1(P, C, i, nU);
+      const θ_ = this.algorithrm1(P, C, i, _U_);
 
       // Find dominant item 𝑑∗:
-      // 𝑑∗ ← argmin𝑑 ∈𝐷𝑖{ 𝜃𝑖𝑑 | 𝜃𝑖𝑑 ≥ 𝜃∗}
-      const d_ = this.findArgMinMax(Di, a_).argMin;
+      const d_ = this.findDominantItem(i, Di, P, θ_);
 
       // Update capacity:
       C = C - w[i][d_];
@@ -80,6 +64,10 @@ export class Optimization {
     return picked_d;
   }
 
+  // =======================> <All sub functions> <=======================
+
+  // Get updated efficiency threshold 𝜃∗:
+  // 𝜃∗ ← A𝑙𝑔𝑜𝑟𝑖𝑡ℎ𝑚1 (𝑃,𝐶,𝑖, |𝑈 |)
   private algorithrm1(
     past_P: PStruct[],
     C: number,
@@ -98,28 +86,105 @@ export class Optimization {
     return _.min(filterd) ?? -1;
   }
 
-  private getF(i: number, P: PStruct[]): number {
-    if (i === 0) {
-      return P[0].w / P.length;
+  // Compute incremental values and weights (𝑣𝑖𝑑,𝑤𝑖𝑑 )
+  private computeIncremental(
+    v: number[][],
+    w: number[][],
+    i: number,
+    d: number
+  ): { w_id: number; v_id: number } {
+    if (d === 0) {
+      return {
+        w_id: w[i][0],
+        v_id: v[i][0],
+      };
     }
 
-    return this.getF(i - 1, P) + P[i].w / P.length;
-  }
-
-  private findArgMinMax(
-    array: number[],
-    threshold: number
-  ): { argMin: number; argMax: number } {
-    const argFact =
-      (compareFn: { (min: any, el: any): any; (max: any, el: any): any }) =>
-      (array: any[]) =>
-        array.map((el, idx) => [el, idx]).reduce(compareFn);
-    const argMax = argFact((min, el) => (el[0] > min[0] ? el : min));
-    const argMin = argFact((max, el) => (el[0] < max[0] ? el : max));
-
     return {
-      argMin: argMin(array).filter((x) => x > threshold)[0] ?? -1,
-      argMax: argMin(array)[0],
+      w_id: w[i][d] - w[i][d - 1],
+      v_id: v[i][d] - v[i][d - 1],
     };
   }
+
+  // Compute efficiency angle 𝜃𝑖𝑑 :
+  private computeEfficiencyAngle(v_id: number, w_id: number): number {
+    if (v_id === 0 && w_id === 0) {
+      return (3 * Math.PI) / 2;
+    } else if (v_id < 0 && w_id <= 0) {
+      return 2 * Math.PI + Math.atan2(v_id, w_id);
+    }
+
+    return Math.atan2(v_id, w_id);
+  }
+
+  // Find dominant item 𝑑∗:
+  private findDominantItem(
+    i: number,
+    Di: number[],
+    P: PStruct[],
+    θ_: number
+  ): number {
+    // 𝑑∗ ← argmin𝑑 ∈𝐷𝑖{ 𝜃𝑖𝑑 | 𝜃𝑖𝑑 ≥ 𝜃∗}
+    const _P = P.filter((x) => x.i === i && x.θ >= θ_);
+    if (Di.length === 0 || _P.length === 0) {
+      return -1;
+    }
+
+    let min = _P[0];
+
+    _P.forEach((p) => {
+      if (p.θ < min.θ) {
+        min = p;
+      }
+    });
+
+    return min.d;
+  }
 }
+
+// private getF(i: number, P: PStruct[]): number {
+//   if (i === 0) {
+//     return P[0].w / P.length;
+//   }
+
+//   return this.getF(i - 1, P) + P[i].w / P.length;
+// }
+
+// private findDominantItem(
+//   i: number,
+//   Di: number[],
+//   P: PStruct[],
+//   θ_: number
+// ): number {
+//   // const d_ = this.argMinMax(Di, θ_).argMin;
+//   const d_ = this.indexOfMin();
+
+//   const argFact =
+//     (compareFn: { (min: any, el: any): any; (max: any, el: any): any }) =>
+//     (array: any[]) =>
+//       array.map((el, idx) => [el, idx]).reduce(compareFn);
+//   const argMax = argFact((min, el) => (el[0] > min[0] ? el : min));
+//   const argMin = argFact((max, el) => (el[0] < max[0] ? el : max));
+
+//   return {
+//     argMin: argMin(array).filter((x) => x > threshold)[0] ?? -1,
+//     argMax: argMin(array)[0],
+//   };
+// }
+
+// private argMinMax(
+//   array: number[],
+//   threshold: number
+// ): { argMin: number; argMax: number } {
+//   const argFact =
+//     (compareFn: { (min: any, el: any): any; (max: any, el: any): any }) =>
+//     (array: any[]) =>
+//       array.map((el, idx) => [el, idx]).reduce(compareFn);
+//   const argMax = argFact((min, el) => (el[0] > min[0] ? el : min));
+//   const argMin = argFact((max, el) => (el[0] < max[0] ? el : max));
+
+//   return {
+//     argMin: argMin(array).filter((x) => x > threshold)[0] ?? -1,
+//     argMax: argMin(array)[0],
+//   };
+// }
